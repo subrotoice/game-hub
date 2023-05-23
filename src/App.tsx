190f132e2144771ced859cea4fa5771fa5b69606
subrotoice <subroto.iu@gmail.com>
2023-05-23 07:24:23 +0600

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <p>We are here</p>
+    </div>
+  );
+}
+
+export default App;
