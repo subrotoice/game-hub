86465778bda9330e7f6dffbbaa016850268af282
subrotoice <subroto.iu@gmail.com>
2023-05-23 09:06:38 +0600

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem } from "@chakra-ui/react";
+import { CircularProgress, CircularProgressLabel } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={`"header header"
+                    "nav main"
+                    "nav footer"`}
+      gridTemplateRows={"50px 1fr 30px"}
+      gridTemplateColumns={"150px 1fr"}
+      h="200px"
+      gap="1"
+      color="blackAlpha.900"
+      fontWeight="bold"
+    >
+      <GridItem pl="2" bg="orange.300" area={"header"}>
+        Header
+      </GridItem>
+      <GridItem pl="2" bg="pink.300" area={"nav"}>
+        Nav
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+      <GridItem pl="2" bg="blue.300" area={"footer"}>
+        Footer
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
